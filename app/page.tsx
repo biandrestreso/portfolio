bf3d2868243e0d4b6cddee065e0b8929ea4d4d57
Biandré Streso <biandre.streso@outlook.com>
2023-06-28 18:15:54 +0200

    Move socials and add navigation links

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,85 @@ export default function Home() {
                 applications, or anything in between. My goal is to always build
                 products that provide pixel-perfect, performant experiences.
               </p>
+              <nav className="nav hidden lg:block">
+                <ul className="flex flex-col pl-4  mt-6">
+                  <li>
+                    <a href="#about" className="group flex items-center py-3">
+                      {/* make a cool line that displays the active state */}
+                      <span className="w-1 h-1 bg-slate-100 rounded-full mr-2"></span>
+                      About
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#experience" className="group flex items-center py-3">
+                      <span className="w-1 h-1 bg-slate-100 rounded-full mr-2"></span>
+                      Experience
+                    </a>
+                  </li>
+                </ul>
+              </nav>
             </div>
+            <ul className="flex items-center gap-6 ml-1 mt-12">
+              <li className="text-xs">
+                <a
+                  href="https://github.com/biandrestreso"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:text-slate-100"
+                >
+                  <span className="sr-only">GitHub</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    className=" h-6 w-6"
+                  >
+                    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                  </svg>
+                </a>
+              </li>
+              <li className="text-xs">
+                <a
+                  href="https://www.linkedin.com/in/biandrestreso/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:text-slate-100"
+                >
+                  <span className="sr-only">LinkedIn</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="h-6 w-6"
+                    aria-hidden="true"
+                  >
+                    <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
+                  </svg>
+                </a>
+              </li>
+              <li className="text-xs">
+                <a
+                  className="block hover:text-slate-200"
+                  href="https://twitter.com/biandrestreso"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span className="sr-only">Twitter</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 248 204"
+                    fill="currentColor"
+                    className="h-6 w-6"
+                    aria-hidden="true"
+                  >
+                    <path
+                      id="white_background"
+                      d="M221.95,51.29c0.15,2.17,0.15,4.34,0.15,6.53c0,66.73-50.8,143.69-143.69,143.69v-0.04   C50.97,201.51,24.1,193.65,1,178.83c3.99,0.48,8,0.72,12.02,0.73c22.74,0.02,44.83-7.61,62.72-21.66   c-21.61-0.41-40.56-14.5-47.18-35.07c7.57,1.46,15.37,1.16,22.8-0.87C27.8,117.2,10.85,96.5,10.85,72.46c0-0.22,0-0.43,0-0.64   c7.02,3.91,14.88,6.08,22.92,6.32C11.58,63.31,4.74,33.79,18.14,10.71c25.64,31.55,63.47,50.73,104.08,52.76   c-4.07-17.54,1.49-35.92,14.61-48.25c20.34-19.12,52.33-18.14,71.45,2.19c11.31-2.23,22.15-6.38,32.07-12.26   c-3.77,11.69-11.66,21.62-22.2,27.93c10.01-1.18,19.79-3.86,29-7.95C240.37,35.29,231.83,44.14,221.95,51.29z"
+                    ></path>
+                  </svg>
+                </a>
+              </li>
+            </ul>
           </header>
           <main className="pt-12 lg:pt-24 lg:w-1/2 lg:py-24">
             <section
@@ -276,67 +354,6 @@ export default function Home() {
           </main>
         </div>
         <footer className="flex flex-col gap-4 items-center justify-center w-full h-24">
-          <ul className="flex items-center gap-6">
-            <li className="text-xs">
-              <a
-                href="https://github.com/biandrestreso"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-slate-100"
-              >
-                <span className="sr-only">GitHub</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  className=" h-6 w-6"
-                >
-                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-                </svg>
-              </a>
-            </li>
-            <li className="text-xs">
-              <a
-                href="https://www.linkedin.com/in/biandrestreso/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-slate-100"
-              >
-                <span className="sr-only">LinkedIn</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="h-6 w-6"
-                  aria-hidden="true"
-                >
-                  <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
-                </svg>
-              </a>
-            </li>
-            <li className="text-xs">
-              <a
-                className="block hover:text-slate-200"
-                href="https://twitter.com/biandrestreso"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <span className="sr-only">Twitter</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 248 204"
-                  fill="currentColor"
-                  className="h-6 w-6"
-                  aria-hidden="true"
-                >
-                  <path
-                    id="white_background"
-                    d="M221.95,51.29c0.15,2.17,0.15,4.34,0.15,6.53c0,66.73-50.8,143.69-143.69,143.69v-0.04   C50.97,201.51,24.1,193.65,1,178.83c3.99,0.48,8,0.72,12.02,0.73c22.74,0.02,44.83-7.61,62.72-21.66   c-21.61-0.41-40.56-14.5-47.18-35.07c7.57,1.46,15.37,1.16,22.8-0.87C27.8,117.2,10.85,96.5,10.85,72.46c0-0.22,0-0.43,0-0.64   c7.02,3.91,14.88,6.08,22.92,6.32C11.58,63.31,4.74,33.79,18.14,10.71c25.64,31.55,63.47,50.73,104.08,52.76   c-4.07-17.54,1.49-35.92,14.61-48.25c20.34-19.12,52.33-18.14,71.45,2.19c11.31-2.23,22.15-6.38,32.07-12.26   c-3.77,11.69-11.66,21.62-22.2,27.93c10.01-1.18,19.79-3.86,29-7.95C240.37,35.29,231.83,44.14,221.95,51.29z"
-                  ></path>
-                </svg>
-              </a>
-            </li>
-          </ul>
           <div className="text-sm text-center text-slate-200">
             <p>Made by Biandré Streso</p>
             <p>twentytwentythree</p>
